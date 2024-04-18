644744d4c744b36b72830704e161ae7934e26f87
Nimish4545 <nimish45bansal@gmail.com>
2024-04-19 01:05:09 +0530

    Option Page - TraveLIT

---
+++ OptionPage.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class OptionPage {
+    JFrame frame;
+    OptionPage(){
+        JPanel optionPanel = new JPanel();
+        optionPanel.setLayout(new GridLayout(2,1));
+
+        JLabel label = new JLabel("Are You A New User?");
+        optionPanel.add(label);
+
+        JPanel buttonPanel = new JPanel();
+
+        JButton loginButton = new JButton("Yes");
+        loginButton.setBounds(10, 80, 80, 35);
+        buttonPanel.add(loginButton);
+
+        JButton registerButton = new JButton("No");
+        registerButton.setBounds(180, 80, 80, 35);
+        buttonPanel.add(registerButton);
+        optionPanel.add(buttonPanel);
+        optionPanel.setAlignmentX(SwingConstants.CENTER);
+        optionPanel.setAlignmentY(SwingConstants.CENTER);
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Implement login logic here
+                new SignUpPage();
+                frame.dispose();
+            }
+        });
+
+        registerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Implement registration logic here
+                new LoginPage();
+                frame.dispose();
+            }
+        });
+
+        frame = new JFrame("User Options | TraveLIT");
+        frame.add(optionPanel);
+        frame.setVisible(true);
+        frame.pack();
+    }
+    public static void main(String args[]) {
+        new OptionPage();
+    }
+}
